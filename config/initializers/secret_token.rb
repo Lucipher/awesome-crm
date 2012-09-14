d73a2a6e50bc41c03f745e7fa58f30d7e59806fa
Edwin Boaz Soenaryo <edwinboaz@gmail.com>
2012-09-14 20:13:29 +0800

    First commit after ActiveAdmin installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRM::Application.config.secret_token = '5e41707ada070f687139ce165fb8a747d7824ac73673632cd9ba5071fadcc2cf0b03bcb9e0508179557076a4e740f8e7d8c5dabb82ed534e9a9ee2bcb46c128f'
